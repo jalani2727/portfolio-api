91fedffd0a77a3078c9782698a8b9c8b9c7e114d
Jalani <jalani2727@gmail.com>
2026-05-07 08:07:45 -0400

    create intial submissions migration

---
+++ src/migrations/002_submissions.js
@@ -0,0 +1,28 @@
+import {sql} from 'kysely';
+
+export async function up(db) {
+    // Create columns for 
+    // id
+    // name
+    // email
+    // phone
+    // message
+    // created_at (timestamp)
+    // status
+    await db.schema
+        .createTable('submissions')
+        .addColumn('id', 'uuid', (col) => col.notNull().defaultTo(sql`gen_random_uuid()`).primaryKey())
+        .addColumn('name', 'text', (col) => col.notNull())
+        .addColumn('email', 'text', (col) => col.notNull())
+        .addColumn('phone', 'text')
+        .addColumn('message', 'text', (col) => col.notNull())
+        .addColumn('created_at', 'timestamptz', (col) => col.notNull().defaultTo(sql`now()`))
+        .addColumn('status', 'text', (col) => col.notNull().defaultTo('new'))
+        .execute();
+}
+
+export async function down(db) {
+    await db.schema
+        .dropTable('submissions')
+        .execute();
+}
