9cdf3181d85d5599df4ba71c88914fa2e6e14b27
Jalani <jalani2727@gmail.com>
2026-05-08 19:31:37 -0400

    Send a notification to my inbox when a successful form submission is made

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import { db } from "./src/db.js";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const resendSandboxEmail = 'onboarding@resend.dev';
+// const domainEmail = 'noreply@jalanipaul.work';
+const myEmail = 'jalani2727@gmail.com';
 
 // The { logger: true } option tells Fastify to print readable logs to your terminal
 const fastify = Fastify({ logger: true });
@@ -45,7 +48,26 @@ fastify.post('/contact', { schema: formRequestSchema }, async (request, reply) =
         message: formData.message,
       })
       .execute();
-
+    // Send a Notification Email to myself
+    await resend.emails.send({
+      from: resendSandboxEmail,
+      to: myEmail,
+      subject: 'jalanipaul.work - Someone is trying to reach you through your portfolio',
+      html: `<p>${formData.name} is trying to get in contact with you.</p>
+      <p>Email: ${formData.email}</p>
+      <p>Phone: ${formData.phone ? formData.phone : 'n/a'}</p>
+      <p>Message: ${formData.message}</p>`
+    })
+    // Send a Notification Email to the user
+    // await resend.emails.send({
+    //   from: domainEmail,
+    //   to: formData.email,
+    //   subject: 'Thank you for reaching out to me - 🍻',
+    //   html: `<p>Hello, ${formData.name} and thank you for taking a look through my portfolio!</p>
+    //   <p>I'll be sure to reach back out to you as soon, as possible.</p>
+    //   <p>Best,</p>
+    //   <p>Jalani</p>`
+    // })
     // fastify.log.info({ formData }, 'Form Data arrived!');
     return { status: 'form submitted' }
 
