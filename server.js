b5f1abe7650963a421d24cc84c83a6dbcf2b99f6
Jalani <jalani2727@gmail.com>
2026-05-08 16:57:07 -0400

    Clean up old comments from initial learnings on servers - add host address to allow networks outside my computer to reach this server (will need to do this eventually when I deploy this server)

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { db } from "./src/db.js";
 
 // The { logger: true } option tells Fastify to print readable logs to your terminal
 const fastify = Fastify({ logger: true });
+
+await fastify.register(cors, {
+  origin: process.env.ALLOWED_ORIGIN,
+})
 
 // Declare a route
 // putting a /health route is good practice for monitoring the status of a server
@@ -50,7 +55,7 @@ fastify.post('/contact', { schema: formRequestSchema }, async (request, reply) =
 // Run the server
 const myServer = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
@@ -58,56 +63,3 @@ const myServer = async () => {
 }
 myServer();
 
-
-
-// // This route responds to POST requests at "/contact".
-// // POST is the HTTP method browsers use when submitting form data.
-// // When your form sends data, this function will run.
-// fastify.post('/contact', async (request, reply) => {
-//   // request.body contains the data the form sent (name, email, phone, message)
-//   console.log('Form data received:', request.body);
-
-//   // For now, just send back a confirmation that we received something.
-//   // Later, this is where we'll save to the database and send an email.
-//   return { success: true, message: 'Form data received' };
-// });
-
-// // This function actually starts the server and tells it which port to listen on.
-// // Port 3000 is a convention for local development — it means the server
-// // is reachable at http://localhost:3000 on your machine.
-// const startServer = async () => {
-//   try {
-//     await fastify.listen({ port: 3000, host: '0.0.0.0' });
-//   } catch (err) {
-//     fastify.log.error(err);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
-// Baby's first server:
-
-
-// const http = require('http');
-// const url = require('url');
-
-
-// const server = http.createServer((req, res) => {
-//   const parsedUrl = url.parse(req.url, true)
-//   if (parsedUrl.pathname === '/') {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.write('yerr, ');
-//     res.end('son');
-//   } else if (parsedUrl.pathname === '/deadass') {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.write('dead');
-//     res.end('ass, son');
-//   } else {
-//     res.writeHead(404, { 'Content-Type': 'text/plain' });
-//     res.end();
-//   }
-
-// })
-
-// server.listen(3000);
